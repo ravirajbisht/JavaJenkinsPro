af4165b40ddf88797bf2827f539bb7d1858c3375
ravirajbisht <ravirajsinghbisht@gmail.com>
2025-06-26 18:45:43 +0530

    first test commit

--- src/main/java/org/ravi/learning/testSomething/AnotherRandomClass.java
+++ src/main/java/org/ravi/learning/testSomething/AnotherRandomClass.java
@@ -1,4 +1,9 @@
 package org.ravi.learning.testSomething;
 
 public class AnotherRandomClass {
+    public int someNumber = 42;
+    public String someString = "Hello, World!";
+    public int sum(int a, int b) {
+        return a + b;
+    }
 }
